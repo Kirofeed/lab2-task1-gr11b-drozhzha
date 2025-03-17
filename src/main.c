0dfc545e5d2cd4ee8ae19e010340cffbd4c5ea79
Kiryl Drozhzha <109621519+Kirofeed@users.noreply.github.com>
2025-03-17 21:34:47 +0000

    Add File Header Comment

--- src/main.c
+++ src/main.c
@@ -1,4 +1,8 @@
-/* hello.c */
+/* main.c
+ * Author: Kiryl Drozhzha
+ * Description: Program that prints "Hello World" and "Goodbye World" to the console.
+ * Date: 2025-03-10
+ */
 #include <stdio.h>
 
 void main (void)
